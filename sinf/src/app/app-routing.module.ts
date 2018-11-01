4d5e4b3652ee8c074ec3ee76fde33640a19db4ab
EdgarACarneiro <up201503784@fe.up.pt>
2018-11-01 22:51:08 +0000

    Base routes

--- sinf/src/app/app-routing.module.ts
+++ sinf/src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FinancialComponent } from './financial/pages/financial/financial.component';
+import { SalesComponent } from './sales/pages/sales/sales.component';
+import { ProcurementComponent } from './procurement/pages/procurement/procurement.component';
+import { OverviewComponent } from './overview/pages/overview/overview.component';
+import { InventoryComponent } from './inventory/pages/inventory/inventory.component';
+import { ProductComponent } from './product/pages/product/product.component';
+import { ClientComponent } from './client/pages/client/client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: OverviewComponent
+  },
+  {
+    path: 'financial',
+    component: FinancialComponent
+  },
+  {
+    path: 'sales',
+    component: SalesComponent
+  },
+  {
+    path: 'procurement',
+    component: ProcurementComponent
+  },
+  {
+    path: 'inventory',
+    component: InventoryComponent
+  },
+  {
+    path: 'product/:id',
+    component: ProductComponent
+  },
+  {
+    path: 'client/:id',
+    component: ClientComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
