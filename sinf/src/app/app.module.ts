6dc91ab258355adb073f72b644acea995e238f65
EdgarACarneiro <up201503784@fe.up.pt>
2018-11-27 15:46:05 +0000

    Added charts packege

--- sinf/src/app/app.module.ts
+++ sinf/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +29,8 @@ import { DashboardComponent } from './shared/pages/dashboard/dashboard.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
