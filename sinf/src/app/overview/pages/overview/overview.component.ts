f1903f08d6d6c3460392a00a6e0bb4ab2aeb7b5d
Sofia Silva <up201505628@fe.up.pt>
2018-11-28 23:26:54 +0000

    overview done :+1: :dancers:

--- sinf/src/app/overview/pages/overview/overview.component.ts
+++ sinf/src/app/overview/pages/overview/overview.component.ts
@@ -12,4 +12,27 @@ export class OverviewComponent implements OnInit {
   ngOnInit() {
   }
 
+  // lineChart
+  public lineChartData: Array<any> = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartType: string = 'line';
+  public pieChartType: string = 'pie';
+
+  // barChart
+  public barChartOptions:any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+ 
+  public barChartData:any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+
 }
