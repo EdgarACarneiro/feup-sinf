11638595fff2b500d8316b9da2c45acf38174a65
Sofia Silva <up201505628@fe.up.pt>
2018-11-28 23:58:39 +0000

    procurement done :astonished:

--- sinf/src/app/sales/pages/sales/sales.component.ts
+++ sinf/src/app/sales/pages/sales/sales.component.ts
@@ -18,17 +18,17 @@ export class SalesComponent implements OnInit {
   public pieChartType: string = 'pie';
 
   // barChart
-  public barChartOptions:any = {
+  public barChartOptions: any = {
     scaleShowVerticalLines: false,
     responsive: true
   };
-  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType:string = 'bar';
-  public barChartLegend:boolean = true;
- 
-  public barChartData:any[] = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  public barChartLabels: string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType: string = 'bar';
+  public barChartLegend: boolean = true;
+
+  public barChartData: any[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
   ];
 
   // lineChart
