0dd9a30909e654fb80acaeb21954a894918b9ffd
Sofia Silva <up201505628@fe.up.pt>
2018-11-30 17:06:23 +0000

    pie chart component :cake:

--- sinf/src/app/shared/components/kpi-pie-chart/kpi-pie-chart.component.ts
+++ sinf/src/app/shared/components/kpi-pie-chart/kpi-pie-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-kpi-pie-chart',
@@ -10,6 +10,36 @@ export class KpiPieChartComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  @Input() private data: number[] = [300, 500, 100];
+
+  @Input() private labels: string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+
+  public options: any = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+    legend: {
+      position: 'bottom'
+    }
+  };
+
+  public type: string = 'pie';
+
+  private colors: any[] = [{
+    backgroundColor: [
+      "rgba(251,64,75,0.5)",
+      "rgba(77,83,96,0.5)",
+      "rgba(251,147,64,0.5)"]
+  }];
+
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
   }
 
 }
