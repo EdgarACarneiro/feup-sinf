e91dde8c449ebfe91647a2f0f32fd755878d680c
EdgarACarneiro <up201503784@fe.up.pt>
2018-12-02 19:51:58 +0000

    Added CashFlow to Finantial View

---
+++ sinf/src/app/financial/components/cash-flow/cash-flow.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { SafTService } from 'src/app/shared/services/safT/saf-t.service';
+
+@Component({
+  selector: 'app-cash-flow',
+  templateUrl: './cash-flow.component.html',
+  styleUrls: ['./cash-flow.component.scss']
+})
+export class CashFlowComponent implements OnInit {
+
+  private cashFlow: number;
+
+  constructor(private saft: SafTService) { }
+
+  ngOnInit() {
+    this.saft.get('GeneralAccounts/AccountID/11').subscribe(
+      (data: Array<any>) => this.cashFlow = (data[0].ClosingDebitBalance - data[0].ClosingCreditBalance)
+    );
+  }
+
+}
