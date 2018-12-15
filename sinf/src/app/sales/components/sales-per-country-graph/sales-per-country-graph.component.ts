c0a8b361b977abf262787d30f68835b56dc1fd16
AndreFCruz <andrecruz97@gmail.com>
2018-12-15 23:05:16 +0000

    sales per region, closes #26

--- sinf/src/app/sales/components/sales-per-country-graph/sales-per-country-graph.component.ts
+++ sinf/src/app/sales/components/sales-per-country-graph/sales-per-country-graph.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { SafTService } from 'src/app/shared/services/safT/saf-t.service';
+import { stringify } from '@angular/core/src/render3/util';
 
 @Component({
   selector: 'app-sales-per-country-graph',
@@ -7,13 +9,39 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SalesPerCountryGraphComponent implements OnInit {
 
-  constructor() { }
+  private salesByRegion: Map<string, number>;
+  private labels: Array<string>;
+  private values: Array<number>;
+
+  constructor(private saft: SafTService) {
+    this.salesByRegion = new Map<string, number>();
+  }
 
   ngOnInit() {
   }
 
   @Input('timeframe')
   set timeframe(frame) {
+
+    this.saft.get(`sales/sales-by-region?start-date=${frame.begin}&end-date=${frame.end}`).subscribe(
+      (data: any) => {
+        data.forEach(element => {
+          this.salesByRegion.set(element.id, parseInt(element.netTotal))
+        });
+        this.updateGraphData(this.salesByRegion);
+      }
+    );
+
+  }
+
+  private updateGraphData(dataMap: Map<string, number>) {
+    this.labels = new Array<string>();
+    this.values = new Array<number>();
+
+    dataMap.forEach((value: number, key: string) => {
+      this.labels.push(key);
+      this.values.push(value);
+    });
   }
 
 }
