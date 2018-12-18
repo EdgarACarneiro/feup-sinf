7da36c34d30d032a1052c9efd51c402d36cf9016
AndreFCruz <andrecruz97@gmail.com>
2018-12-18 23:15:54 +0000

    updated balance sheet KPI

--- sinf/src/app/financial/components/balance-sheet/balance-sheet.component.ts
+++ sinf/src/app/financial/components/balance-sheet/balance-sheet.component.ts
@@ -18,15 +18,31 @@ export class BalanceSheetComponent implements OnInit {
   private totalDebit: number = 0;
   private totalCredit: number = 0;
 
+  private accountNames:Map<string, string> = new Map([
+    ["11", "Caixa"],
+    ["12", "Depósitos à Ordem"],
+    ["21", "Contas a Receber de Clientes"],
+    ["22", "Contas a Pagar a Fornecedores"],
+    ["24", "Estado e Outros Entes Públicos"],
+    ["31", "Compras"],
+    ["36", "Produtos e Trabalhos em Curso"],
+    ["61", "Custo das Mercadorias Vendidas"],
+    ["62", "Fornecimentos e Serviços Externos"],
+    ["71", "Vendas"],
+    ["72", "Prestações de Serviços"]
+  ]);
+
+
   constructor(private saft: SafTService) { }
 
   ngOnInit() {
     for (let i = 11; i <= 81; i++) {
       this.saft.get('AccountSum/' + i.toString()).subscribe(
         (data: Object) => {
+          let id = i.toString()
           if (data['totalDebit'] != 0 || data['totalCredit'] != 0) {
             this.accounts.push({
-              id: i,
+              id: this.accountNames.has(id) ? this.accountNames.get(id) : id,
               debit: data['totalDebit'],
               credit: data['totalCredit']
             })
